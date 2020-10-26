b5a4546e2a5bb2d5594d618439758d6d1eacd49f
vsc43752 vscuser <vsc43752@gligar04.gastly.os>
2020-10-26 19:42:57 +0100

    K_mesh 13

--- task1/kmesh_Fe4N.sh
+++ task1/kmesh_Fe4N.sh
@@ -5,7 +5,7 @@
 #PBS -o outputFe4N_Kmesh.file
 #PBS -e errorFe4N_Kmesh.file
 #PBS -l walltime=1:00:00
-#PBS -l nodes=1:ppn=1
+#PBS -l nodes=1:ppn=6
 #PBS -m be -M emile.segers8@gmail.com
 #
 # change to directory you were working when submitting job
@@ -13,4 +13,4 @@ cd $PBS_O_WORKDIR
 #load QE
 module load QuantumESPRESSO/5.2.1-intel-2015b
 #run QE command
-mpirun -np $PBS_NP ./auto_K_mesh.sh
+mpirun -np $PBS_NP pw.x -input config.in > config.out
