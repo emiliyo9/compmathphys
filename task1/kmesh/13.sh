cc38f9380485037e88bd9ff243962982d7271dae
vsc43752 vscuser <vsc43752@gligar04.gastly.os>
2020-10-26 21:14:02 +0100

    Mistake made in kmesh 5

--- task1/kmesh/13.sh
+++ task1/kmesh/13.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 #
-#PBS -N jobFe4N_Kmesh
-#PBS -o outputFe4N_Kmesh.file
-#PBS -e errorFe4N_Kmesh.file
+#PBS -N kmesh13
+#PBS -o out_kmesh13.file
+#PBS -e error_kmesh13.file
 #PBS -l walltime=1:00:00
 #PBS -l nodes=1:ppn=6
 #PBS -m be -M emile.segers8@gmail.com
