2b7727163f2d1643bd1ba7bc14ee65897d46ae09
epsegers <emile.segers8@gmail.com>
2020-10-27 13:50:46 +0100

    add setup for factor optimization

--- task1/factor/8.sh
+++ task1/factor/8.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 #
-#PBS -N factor1
-#PBS -o out_factor1.file
-#PBS -e error_factor1.file
+#PBS -N factor8
+#PBS -o out_factor8.file
+#PBS -e error_factor8.file
 #PBS -l walltime=1:00:00
 #PBS -l nodes=1:ppn=6
 #PBS -m be -M emile.segers8@gmail.com
@@ -13,4 +13,4 @@ cd $PBS_O_WORKDIR
 #load QE
 module load QuantumESPRESSO/5.2.1-intel-2015b
 #run QE command
-mpirun -np $PBS_NP pw.x -input 1.in > 1.out
+mpirun -np $PBS_NP pw.x -input 8.in > 8.out
---
+++ task1/factor/setup_factor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+w=$(grep 'twfc' config.in | grep -o '[0-9]*')
+r=$(grep 'trho' config.in | grep -o '[0-9]*')
+for (( i = 2; i < 11; ++i )); do
+    # setup input file for pw.x
+    
+    $(cp config.in $i.in)
+    sedEcutrho=$(grep -n "trho" $i.in | grep -o "^[0-9]*")"s/$r/"$(echo $w"*"$i | bc)"/g"
+    echo $sedEcutrho
+    $(sed -i $sedEcutrho $i.in)
+    
+    $(cp config.sh $i.sh)
+    $(sed -i "4,6s/1/$i/g" $i.sh)
+    $(sed -i "16s/1/$i/g" $i.sh)
+
+done
