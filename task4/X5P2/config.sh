426d66b07ed6426287c537cc60017c528609a3be
vsc43752 vscuser <vsc43752@gligar05.gastly.os>
2020-11-14 14:53:06 +0100

    X5P2 results at 30GPa

--- task4/X5P2/config.sh
+++ task4/X5P2/config.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
 #
-#PBS -N bss1
-#PBS -o out_bss1.file
-#PBS -e error_bss1.file
+#PBS -N X5P2
+#PBS -o out_x5p2.file
+#PBS -e error_x5p2.file
 #PBS -l walltime=24:00:00
-#PBS -l nodes=1:ppn=12
+#PBS -l nodes=1:ppn=24
 #PBS -m be -M emile.segers8@gmail.com
 #
 # change to directory you were working when submitting job
