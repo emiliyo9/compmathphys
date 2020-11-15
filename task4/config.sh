4457662a0a7cec9b80c13f85862a452c9ffcf723
epsegers <emile.segers8@gmail.com>
2020-11-15 18:32:06 +0100

    mistake in phonon .in files

---
+++ task4/config.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+#
+#PBS -N phonon_calc
+#PBS -o out.file
+#PBS -e err.file
+#PBS -l walltime=12:00:00
+#PBS -l nodes=1:ppn=24
+#PBS -m be -M emile.segers8@gmail.com
+#
+# change to directory you were working when submitting job
+cd $PBS_O_WORKDIR
+#load QE
+module load QuantumESPRESSO/5.2.1-intel-2015b
+#run QE command
+mpirun -np $PBS_NP pw.x -input X5P2/config.in > X5P2/config.out
+mpirun -np $PBS_NP pw.x -input X5C1/config.in > X5C1/config.out
